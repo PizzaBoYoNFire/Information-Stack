3435fbdd0f33eb3650abafc3363a0de2067d556e
Diwakar <dekkaladiwakar@gmail.com>
2020-02-18 20:08:26 +0530

    express server with mongoDB connected

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//db config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongoDB
+mongoose
+  .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then( () => console.log("mongoDB connected."))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('HelloWorld'));
+
+// use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
